04b49a251f19ae36750f105e7cc731c7d3a1a1d7
Nathaniel Mosco <natemosco@gmail.com>
2019-12-19 04:18:31 -0500

    added gitignore from guided finish server.js

---
+++ api/server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const helmet = require("helmet");
+const cors = require("cors");
+
+const authRouter = require("../auth/auth-router");
+const usersRouter = require("../users/users-router");
+
+const globalMiddleware = [helmet(), cors(), express.json()];
+
+const server = express();
+server.use(globalMiddleware);
+server.use("/api/auth", authRouter);
+server.use("./api/users", usersRouter);
+server.get("/", (req, res) => {
+  res.send("server is up and running.");
+});
+
+module.exports = server;
